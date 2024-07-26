da5df175fb150abe43186f8efed941beeb80b893
vascoalexander <vascoalexander@gmail.com>
2024-07-26 21:56:32 +0200

    exercise 1-5-6 alternative solution

--- Python/Basics/Miscellaneous/Exercise_1-5-6.py
+++ Python/Basics/Miscellaneous/Exercise_1-5-6.py
@@ -14,6 +14,20 @@ else:
             break
     if prime_number == True:
         print(f'{number} is a prime number.')
+
+# shorter alternative with for-else block
+number = int(input('Enter a positive number higher than 1: '))
+
+if number < 2:
+    print('Invalid Input')
+else:
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            print(f'{number} is not a prime number')
+            break
+    else:
+        print(f'{number} is a prime number.')
+
     
     
 
