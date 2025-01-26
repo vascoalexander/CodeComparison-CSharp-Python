5b6c688ad64aa5e52e6c0fa2267be43ea39e3ab4
vascoalexander <vascoalexander@gmail.com>
2025-01-26 09:38:32 +0100

    fix error in 1-3-9

--- CSharp/Basics/Loops/Exercise 1-3-6/Exercise 1-3-9/Program.cs
+++ CSharp/Basics/Loops/Exercise 1-3-6/Exercise 1-3-9/Program.cs
@@ -1,16 +1,27 @@
 ﻿// Exercise 1-3-9
-for (int row = 0; row < 7; row++)
+namespace Exercise_1_3_9
 {
-    for (int col = 0; col < 7; col++)
-    {   
-        if (row == col)
+    internal class Program
+    {
+        static void Main()
         {
-            Console.Write("X");
-        }
-        else
-        {
-            Console.Write("-");
+
+            for (int row = 0; row < 7; row++)
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    if (row == col)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("-");
+                    }
+                }
+
+                Console.WriteLine("");
+            }
         }
     }
-    Console.WriteLine("");
 }
